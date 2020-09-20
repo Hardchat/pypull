1d6a29053ac6ece2ded6484837967bef192b9906
Hardchat <freakthehacker@protonmail.com>
2020-09-20 03:38:23 -0400

    Update pypull.py

--- pypull.py
+++ pypull.py
@@ -1,20 +1,24 @@
 from scapy.all import *
-import ipinfo
+import ipinfo, sys
 
-f = open('filter.txt', 'r')
-filters = f.read()
+try:
+   urfile = sys.argv[1]
+   f = open(urfile, 'r')
+   filters = f.read()  
+except:
+   print('Error!\n\nUsage: pull [filter.txt]\n')
+else:
+   def print_summary(pkt):
+      if IP in pkt:
+         ip_src=pkt[IP].src
+      if UDP in pkt:
+         udp_sport=pkt[UDP].sport
+         access_token = '5ebbb1dee38bf6'
+         handler = ipinfo.getHandler(access_token)
+         match = handler.getDetails(ip_src)
+         c = match.details.get('city')
+         s = match.details.get('region')
+         strang = ('IP ' + str(ip_src) + ' ' + str(udp_sport) + ' ' + str(c) + ', ' + str(s))
+         print(strang)
 
-def print_summary(pkt):
-   if IP in pkt:
-      ip_src=pkt[IP].src
-   if UDP in pkt:
-      udp_sport=pkt[UDP].sport
-      access_token = '5ebbb1dee38bf6'
-      handler = ipinfo.getHandler(access_token)
-      match = handler.getDetails(ip_src)
-      c = match.details.get('city')
-      s = match.details.get('region')
-      strang = ("IP " + str(ip_src) + " " + str(udp_sport) + " " + str(c) + ", " + str(s))
-      print(strang)
-
-sniff(filter=filters, prn=print_summary)
+   sniff(filter=filters, prn=print_summary)
