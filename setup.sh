ce51414bc9df871eb82691394668c4e736bd47c9
Hardchat <68983799+Hardchat@users.noreply.github.com>
2020-10-03 14:06:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ apt install python3-pip -y;
 pip3 install scapy;
 pip3 install ipinfo;
 echo "alias pull='python3 pypull.py'" >> ~/.bashrc
-echo "alias pull='python3 pypull/pypull.py'" >> ~/.bashrc
+echo "alias pull='python3 ~/pypull/pypull.py'" >> ~/.bashrc
