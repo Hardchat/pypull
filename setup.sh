e058ad28226a3ac46f42fe44abd4ee4f051a85be
Hardchat <68983799+Hardchat@users.noreply.github.com>
2021-05-09 04:42:55 -0400

    YUM and APT package manager support.
    Allows required dependencies to be installed according to the host's package manager.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
-apt install python3 -y;
-apt install python3-pip -y;
-pip3 install scapy;
-pip3 install ipinfo;
-echo "alias pull='python3 pypull.py'" >> ~/.bashrc
-echo "alias pull='python3 ~/pypull/pypull.py'" >> ~/.bashrc
+#!/bin/bash
+
+APT_PACKAGES='build-essential python3 python3-pip'
+YUM_PACKAGES='wget curl tcpdump gcc gcc-c++ make python3-devel python3 python3-pip'
+PIP_PACKAGES='scapy ipinfo'
+has_apt() {
+         apt update && apt upgrade -y; apt-get install $APT_PACKAGES -y && pip3 install $PIP_PACKAGES; echo "alias pull='python3 pypull.py'" >> ~/.bashrc && echo "alias pull='python3 ~/pypull/pypull.py'" >> ~/.bashrc;
+}
+
+has_yum() {
+         yum update -y; yum install $YUM_PACKAGES -y && pip3 install $PIP_PACKAGES; echo "alias pull='python3 pypull.py'" >> ~/.bashrc && echo "alias pull='python3 ~/pypull/pypull.py'" >> ~/.bashrc;
+}
+
+package() {
+    [ -x "$(which $1)" ]
+}
+
+if package apt-get ; then has_apt
+elif package yum ; then has_yum
+else
+    echo 'Unknown package manager! Manual dependency installation required.'
+    exit 2
+fi
