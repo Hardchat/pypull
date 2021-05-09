53e9ffe34c31c0c104be9fd2a2f9871a2cbf2109
Hardchat <68983799+Hardchat@users.noreply.github.com>
2021-05-09 04:38:19 -0400

    Supports TCP and UDP.

--- pypull.py
+++ pypull.py
@@ -1,43 +1,47 @@
+import sys
+import ipinfo
 from scapy.all import *
-import ipinfo, sys
 
 seen = set()
 
 try:
    #ipinfo token goes below here in the quotes
-   urtoken = ''
+    urtoken = ''
    #filter file supplied in terminal
-   urfile = os.path.join(sys.path[0], 'filters/', sys.argv[1])
-   
-   f = open(urfile, 'r')
-   
-   filters = f.read()  
+    urfile = os.path.join(sys.path[0], 'filters/', sys.argv[1])
 
-except:
-   muhpath = sys.path[0] + '/filters'
-   urfilters = os.listdir(muhpath)
-   print('Error, Usage: pull [filter.txt]\n')
-   print('Available Filters Located in [pypyull/filters] are:\n')
+    f = open(urfile, 'r')
+
+    if os.stat(urfile).st_size == 0:
+
+        filters = "not icmp"
+    else:
+
+        filters = "not icmp and " + f.read()
+except IOError:
+    filter_path = sys.path[0] + '/filters'
+    urfilters = os.listdir(filter_path)
+    print('Error, Usage: pull [filter.txt]\n')
+    print('Available Filters Located in [pypyull/filters] are:\n')
    #listing the files within pypull/filters
-   for f in urfilters:
-      print(f'[{f}]', end = ' ')
-   print('\n')
-   
+    for f in urfilters:
+        print(f'[{f}]', end=' ')
+    print('\n')
+
 else:
-   
-   def print_summary(pkt):
-      if IP in pkt:
-         ip_src=pkt[IP].src
-         if UDP in pkt and ip_src not in seen:
-            seen.add(ip_src)
-            udp_sport=pkt[UDP].sport
-            access_token = urtoken
-            handler = ipinfo.getHandler(access_token)
-            match = handler.getDetails(ip_src)
-            c = match.details.get('city')
-            s = match.details.get('region')
-            strang = ('IP ' + str(ip_src) + ' ' + str(udp_sport) + ' (' + str(c) + ', ' + str(s) + ')')
-            print(strang)
-   print('Started Listening!')
-   sniff(filter=filters, prn=print_summary)
-   
+
+    def print_summary(pkt):
+        if IP in pkt:
+            host_src = pkt[IP].src
+            if host_src not in seen:
+                seen.add(host_src)
+                host_sport = pkt[IP].sport
+                access_token = urtoken
+                handler = ipinfo.getHandler(access_token)
+                match = handler.getDetails(host_src)
+                city = match.details.get('city')
+                region = match.details.get('region')
+                strang = ('IP ' + str(host_src) + ' ' + str(host_sport) + ' (' + str(city) + ', ' + str(region) + ')')
+                print(strang)
+    print('Started Listening!')
+    sniff(filter=filters, prn=print_summary)
